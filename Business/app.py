37cddf510122acec689a511d4297d2d98432083f
TzuHsuanLin <zixuanlin59@gmail.com>
2023-02-11 01:26:50 +0800

    feat: 打storage api

--- Business/app.py
+++ Business/app.py
@@ -5,17 +5,19 @@ from flask import Flask, request
 from flask_cors import CORS
 import requests
 
-from config import INVENTORY_URL
+from config import INVENTORY_URL,STORAGE_URL
 app = Flask(__name__)
 CORS(app)
 URL_MATERIAL = f'{INVENTORY_URL}/material'
+URL_SAVE = f'{STORAGE_URL}/records'
 @app.route('/order',methods=['POST'])
 def order():
     data = request.json
     response = requests.post(url=URL_MATERIAL,json=data.get('data'))
     z = data.copy()
     z.update(response.json())
-    return z
+    print(z)
+    return requests.post(url=URL_SAVE,json=z).text
 
 
 @app.route('/')
