f714828dcdd7614f8a937412026d7a04617074a6
TzuHsuanLin <zixuanlin59@gmail.com>
2023-02-10 23:17:17 +0800

    feat: Inventory complete router

--- Inventory/app.py
+++ Inventory/app.py
@@ -1,23 +1,45 @@
 from crypt import methods
 import json
 from urllib import response
-from flask import Flask, request
+from flask import Flask, jsonify, request,abort
 from flask_cors import CORS
 import requests
+import logging
+import base64
 
 app = Flask(__name__)
 CORS(app)
+formula = {'a':3,'b':2,'c':4,'d':10}
 # URL_MATERIAL = f'{INVENTORY_URL}/material'
 @app.route('/material',methods=['POST'])
 def order():
     print('RRRRR')
-    # data = request.json
-    # print(data.get('data'))
-    # # response = requests.post(url=URL_MATERIAL,json=data.get('data'))
-    # print(data.get('location'))
-    # print(data.get('timestamp'))
-    # print(data)
-    return 'True'
+    data = request.json
+    try:
+        print(data)
+        material = 0
+        total = 0
+        for item,count in data.items():
+            print(item)
+            print(count)
+            print(item,formula.get(item))
+            material += formula.get(item)*count
+            print(material)
+            total += count
+            
+        return jsonify(
+                material = material,
+                signature = _signature(total)
+            )
+    except Exception as error:
+        abort(500,error)
+    return jsonify(
+        material = 0,
+        signature = _signature(0)
+        )
+
+def _signature(total):
+    return base64.b64encode(str(total).encode('UTF-8')).decode('UTF-8')
 
 
 @app.route('/')
