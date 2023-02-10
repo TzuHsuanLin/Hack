c56f2dff966d40e1cb282835182e9d4b79778135
TzuHsuanLin <zixuanlin59@gmail.com>
2023-02-11 03:53:55 +0800

    feat:add report、record

--- Storage/app.py
+++ Storage/app.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from sqlite3 import Timestamp
 from urllib import response
 from venv import create
 from flask import Flask, jsonify, request,abort,g
@@ -6,6 +7,7 @@ from flask_cors import CORS
 import os
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import true
 app = Flask(__name__)
 CORS(app)
 #  取得目前文件資料夾路徑
@@ -38,6 +40,17 @@ class data(db.Model):
         self.b = b
         self.c = c
         self.d = d
+    def toJSON(self):
+        return{
+            'location': self.location, 
+            'timestamp': self.timestamp.strftime("%Y-%m-%dT%H:%M:%S%z"),
+            'signature': self.signature,
+            'material': self.material,
+            'a':self.a, 
+            'b':self.b, 
+            'c':self.c, 
+            'd':self.d 
+        }
 
 @app.route('/')
 def index():
@@ -46,35 +59,62 @@ def index():
         db.create_all()
     return 'ok'
 
-@app.route('/records',methods=['POST','GET'])
+@app.route('/records',methods=['POST'])
 def save():
-    if request.method == 'POST':
-        response = request.json
-        print(response.get('location'))
-        print(response.get('data').get('a'))
-        print(response.get('timestamp'))
-        date = datetime.strptime(response.get('timestamp'), "%Y-%m-%dT%H:%M:%S%z")
-        
-        print("date:", date.strftime("%Y-%m-%dT%H:%M:%S%z"))
-        datas = data(
-            location = response.get('location'),
-            timestamp = date,
-            signature = response.get('signature'),
-            material = response.get('material'),
-            a = response.get('data').get('a'),
-            b = response.get('data').get('b'),
-            c = response.get('data').get('c'),
-            d = response.get('data').get('d')
-        )
-        
-        try: 
-            db.session.add(datas)
-            db.session.commit()
-            return 'ok'
-        except Exception as error:
-            abort(500,error)
+    response = request.json   
+    date = datetime.strptime(response.get('timestamp'), "%Y-%m-%dT%H:%M:%S%z")
+    
+    datas = data(
+        location = response.get('location'),
+        timestamp = date,
+        signature = response.get('signature'),
+        material = response.get('material'),
+        a = response.get('data').get('a'),
+        b = response.get('data').get('b'),
+        c = response.get('data').get('c'),
+        d = response.get('data').get('d')
+    )
+    try: 
+        print("date:", datas.timestamp.strftime("%Y-%m-%d"))
+        db.session.add(datas)
+        db.session.commit()
+        return 'ok'
+    except Exception as error:
+        abort(500,error)
+from sqlalchemy import func
+@app.route('/records',methods=['GET'])
+def records():
+    location = request.args['location']
+    date = request.args['date']
+    d_date = datetime.strptime(date,"%Y-%m-%d")
+    datas = data.query.filter_by(location = location ,timestamp = d_date).all()
+    if datas is None:
+        return []
+    detail_data = [data.toJSON() for data in datas]
+    return detail_data
+
+
+@app.route('/report',methods=['GET'])
+def report():
+    location = request.args['location']
+    date = request.args['date']
+    d_date = datetime.strptime(date,"%Y-%m-%d")
+    datas = data.query.filter_by(location = location ,timestamp = d_date).all()
+    if datas is None:
+        data_list = {}
     else:
-        return 0
+        data_list = {
+            "location" : location,
+            "date" : date,
+            "count" : len(datas),
+            "material" : sum(r.material for r in datas),
+            "a" : sum(r.a for r in datas),
+            "b" : sum(r.b for r in datas),
+            "c" : sum(r.c for r in datas),
+            "d" : sum(r.d for r in datas)
+        }
+    return data_list
+        
 
 
 
